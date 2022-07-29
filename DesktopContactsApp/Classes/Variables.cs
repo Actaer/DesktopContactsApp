5e7624678b6b48d0c21b8c66b810c44929b89a0b
Adnan Crnovrsanin <actaer@pop-os.localdomain>
2022-07-29 17:28:32 +0200

    Initial commit

---
+++ DesktopContactsApp/Classes/Variables.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopContactsApp.Classes
+{
+    internal class Variables
+    {
+        static string databaseName = "Contacts.db";
+        static string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        public static string databasePath = System.IO.Path.Combine(folderPath, databaseName);
+    }
+}
---
+++ DesktopContactsApp/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using DesktopContactsApp.Classes;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopContactsApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Contact> contacts;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            contacts = new List<Contact>();
+
+            ReadDatabase();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NewContactWindow newContactWindow = new NewContactWindow();
+            newContactWindow.ShowDialog();
+
+            ReadDatabase();
+        }
+
+        void ReadDatabase()
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(Variables.databasePath))
+            {
+                conn.CreateTable<Contact>();
+                contacts = conn.Table<Contact>().ToList();
+            }
+
+            if (contacts != null)
+                contactsListView.ItemsSource = contacts;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            searchTextBox.Background = null;
+
+            TextBox searchQuery = sender as TextBox;
+
+            var filteredList = contacts.Where(c => c.Name.ToLower().Contains(searchQuery.Text.ToLower())).ToList();
+
+            contactsListView.ItemsSource = filteredList;
+        }
+
+        private void contactsListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Contact selectedContact = contactsListView.SelectedItem as Contact; 
+            if (selectedContact != null)
+            {
+                ContactDetailsWindow newContactDetailsWindow = new ContactDetailsWindow(selectedContact);
+                newContactDetailsWindow.ShowDialog();
+            }
+
+            ReadDatabase();
+        }
+    }
+}
